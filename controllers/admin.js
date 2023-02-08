f725cb9b511aa6d2d464cf9deccefabc2e3088c5
abhishekk-web <abhi.chatterjee38@gmail.com>
2023-02-08 22:42:51 +0530

    Associations are now added

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,7 +13,7 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({title: title, imageUrl: imageUrl, price: price, description: description})
+  req.user.createProduct({title: title, imageUrl: imageUrl, price: price, description: description, userId: req.user.id})
   .then((result)=>{
     console.log(result);
     res.redirect('/admin/products')
@@ -30,9 +30,10 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
+  req.user.getProducts({where: {id:prodId}})
   .then(product => {
     if (!product) {
+      const product = product[0];
       return res.redirect('/');
     }
     res.render('admin/edit-product', {
@@ -72,7 +73,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user.getProducts()
   .then(products => {
     res.render('admin/products', {
       prods: products,
