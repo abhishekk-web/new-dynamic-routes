e41fcab25265a3271993f091fc606a5c8f6b8e00
abhishekk-web <abhi.chatterjee38@gmail.com>
2023-02-08 23:24:30 +0530

    cart is now added

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
+
 const sequelize = require('./util/database');
 
 const errorController = require('./controllers/error');
@@ -40,6 +43,10 @@ app.use(errorController.get404);
 
 User.hasMany(Product);
 Product.belongsTo(User);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, {through: CartItem});
+Product.belongsToMany(Cart, {through: CartItem});
 
 sequelize
 // .sync({force:true})
@@ -54,7 +61,9 @@ sequelize
   return user;
 })
 .then((user) => {
-  console.log(user);
+  return user.createCart();
+})
+.then(cart => {
   app.listen(3000);
 })
 .catch(err => {
